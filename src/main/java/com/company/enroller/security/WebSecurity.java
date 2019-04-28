b33ec0cd899da3d9a822bbd79308bacd2ede9371
Oliwia <olakatosz18@gmail.com>
2019-04-28 13:44:56 +0200

    JWT properties, almost working, missing .addFilterBefore

--- src/main/java/com/company/enroller/security/WebSecurity.java
+++ src/main/java/com/company/enroller/security/WebSecurity.java
@@ -1,19 +1,34 @@
 package com.company.enroller.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Autowired
+    ParticipantProvider participantProvider;
+
    @Override
    protected void configure(HttpSecurity http) throws Exception {
        http.csrf().disable()
                .authorizeRequests()
-               .anyRequest().permitAll()
                // .anyRequest().permitAll() - zachowuje wszystko po staremu, permit all itp - trza bylo dodac po zmianie dependency bo nie puszczalo nikogo
+               .anyRequest().permitAll()
                .and()
                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(participantProvider).passwordEncoder(passwordEncoder);
+    }
 }
