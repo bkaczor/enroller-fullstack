bb57e5d18542e9ef3265632dffec4f829257f789
Oliwia <olakatosz18@gmail.com>
2019-05-04 08:46:11 +0200

    aythorization with JWT

--- src/main/java/com/company/enroller/security/WebSecurity.java
+++ src/main/java/com/company/enroller/security/WebSecurity.java
@@ -2,6 +2,7 @@ package com.company.enroller.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,12 +13,11 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private ParticipantProvider participantProvider;
 
     @Autowired
-    PasswordEncoder passwordEncoder;
-
-    @Autowired
-    ParticipantProvider participantProvider;
+    private PasswordEncoder passwordEncoder;
 
     @Value("${security.secret}")
     private String secret;
@@ -29,14 +29,17 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     private int tokenExpiration;
 
     @Override
-   protected void configure(HttpSecurity http) throws Exception {
-       http.csrf().disable()
-               .authorizeRequests()
-               .anyRequest().permitAll()
-               .and()
-               .addFilterBefore(new JWTAuthenticationFilter(authenticationManager(), secret, issuer, tokenExpiration), UsernamePasswordAuthenticationFilter.class)
-               .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-   }
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/api/participants").permitAll()
+                .antMatchers("/api/tokens").permitAll()
+                .antMatchers("/api/**").authenticated()
+                .and()
+                .addFilterBefore(new JWTAuthenticationFilter(authenticationManager(), secret, issuer, tokenExpiration), UsernamePasswordAuthenticationFilter.class)
+                .addFilter(new JWTAuthorizationFilter(authenticationManager(), secret))
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
 
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
